e52de2d71aacd46290eb4477113d318067b3495f
ankit_panwar <34816999+ankitpanwar15@users.noreply.github.com>
2018-09-03 18:06:00 +0530

    final changes

---
+++ app/src/main/java/com/example/anshu/parse/Starter.java
@@ -0,0 +1,53 @@
+package com.example.anshu.parse;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class Starter extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("")
+                .clientKey("")
+                .server("")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("anyClassName", "anyValue");
+
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
